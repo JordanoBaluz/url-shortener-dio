522558e2f359124ad3eff63ccce622c48365744d
Jordano Baluz <35078514+JordanoBaluz@users.noreply.github.com>
2021-11-02 20:37:50 -0300

    Update Constants.ts

--- src/config/Constants.ts
+++ src/config/Constants.ts
@@ -1,5 +1,5 @@
 export const config = {
     API_URL: "http://localhost:5000",
     MONGO_CONNECTION:
-        'mongodb+srv://root:root@url-shortaner-dio.udb5u.mongodb.net/url-shortaner-dio?retryWrites=true&w=majority'
+        'mongodbURL'
 }
